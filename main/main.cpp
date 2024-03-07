989479a8745599bb951780ed21fadd782893e723
suhith17 <suhithgowda17@gmail.com>
2024-03-07 20:45:46 +0530

    Create main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+// File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG21CS547";
+  return 0;
+}
